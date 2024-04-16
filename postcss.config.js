2158727a282b31778ac4fb653e650858bbe15ec9
Caleb Jackson <calebjackson2002@gmail.com>
2024-04-16 13:19:39 -0700

    optimization attemps

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,9 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
+import purgecss from '@fullhuman/postcss-purgecss';
 
-module.exports = {
+export default {
   plugins: [
     purgecss({
-      // Include paths to all template files in your project
-      content: [
-        './**/*.html', // Path to your HTML files
-        './src/**/*.jsx' // Path to your JSX files (adjust the path as necessary)
-      ],
-      // This is necessary to extract classes from within JSX files
+      content: ['./**/*.html', './src/**/*.jsx'],
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
     })
   ]
