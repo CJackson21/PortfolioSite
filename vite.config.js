ffce389a2273e7bc13965576abeef3c5923a37e3
Caleb Jackson <calebjackson2002@gmail.com>
2024-04-16 14:09:12 -0700

    optimization attemps

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-
-// Custom plugin to force MIME type
-function CustomMimePlugin() {
-  return {
-    name: 'configure-server',
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        if (req.url.endsWith('.jsx')) {
-          res.setHeader('Content-Type', 'application/javascript');
-        }
-        next();
-      });
-    },
-  };
-}
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), CustomMimePlugin()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       input: {
