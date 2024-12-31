eb573ab1841e39f41b23de7ebb5876f35bb2f37b
Caleb Jackson <calebj@tzmedical.com>
2024-12-31 14:34:46 -0800

    chore: home page slight adjustments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     <Box sx={{ position: 'relative', height: '100vh' }}>
       <Box
         sx={{
-          height: '100%',
+          height: '100vh',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
