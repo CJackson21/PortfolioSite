dc188a217ed046e31b0b89cf4d4963288d62c03d
Caleb Jackson <calebj@tzmedical.com>
2024-12-31 12:03:38 -0800

    chore: projects centering mobile

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,99 +35,123 @@ function Projects({ isMobile }) {
         textAlign: 'center',
       }}
     >
-      <Typography
-        variant='h3'
-        fontWeight='bold'
+      <Grid
+        container
+        spacing={4}
+        justifyContent='center' // Ensures horizontal centering
+        alignItems='center' // Ensures vertical centering
         sx={{
-          color: theme.palette.text.primary,
-          marginBottom: '3vh',
-          letterSpacing: '0.2vw',
+          maxWidth: '80vw',
+          flexGrow: 1,
+          padding: '5vh',
+          textAlign: 'center',
+          margin: '0 auto', // Centers the container itself
         }}
       >
-        My Projects
-      </Typography>
-      <Grid container spacing={4}>
-        {projects.map((project, index) => (
-          <Grid size={{ xs: 12, sm: 6, md: 4 }} key={index}>
-            <Card
+        <Typography
+          variant='h3'
+          fontWeight='bold'
+          sx={{
+            color: theme.palette.text.primary,
+            marginBottom: '3vh',
+            letterSpacing: '0.2vw',
+          }}
+        >
+          My Projects
+        </Typography>
+        <Grid container spacing={4} justifyContent='center'>
+          {projects.map((project, index) => (
+            <Grid
+              item
+              xs={12} // Full width on extra-small screens
+              sm={6} // Half width on small screens
+              md={4} // One-third width on medium and larger screens
+              key={index}
               sx={{
                 display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                height: '100%',
+                justifyContent: 'center', // Centers each project card
               }}
             >
-              <CardContent>
-                <Box
-                  sx={{
-                    height: { xs: '8rem', sm: '10rem' },
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    marginBottom: '1rem',
-                  }}
-                >
-                  <img
-                    src={project.image}
-                    alt={`${project.title} Logo`}
-                    style={{
-                      maxHeight: '100%',
-                      maxWidth: '100%',
-                      objectFit: 'contain',
-                    }}
-                  />
-                </Box>
-                <Typography
-                  variant='h4'
-                  gutterBottom
-                  sx={{
-                    color:
-                      theme.palette.mode === 'dark'
-                        ? theme.palette.text.primary
-                        : theme.palette.text.secondary,
-                    fontWeight: theme.typography.fontWeightBold,
-                    fontSize: { xs: '1.25rem', sm: '1.5rem' },
-                  }}
-                >
-                  {project.title}
-                </Typography>
-                {isMobile ? (
-                  <Button
-                    variant='contained'
+              <Card
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                  height: '100%',
+                  maxWidth: '20rem', // Optional: limits card width for consistent appearance
+                }}
+              >
+                <CardContent>
+                  <Box
                     sx={{
-                      backgroundColor: theme.palette.threejsback.default,
-                      borderRadius: '20px',
-                      padding: '0.5rem 1.5rem',
-                      fontSize: '0.875rem',
-                      textTransform: 'none',
-                      '&:hover': {
-                        backgroundColor: theme.palette.primary.dark,
-                      },
+                      height: { xs: '8rem', sm: '10rem' },
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      marginBottom: '1rem',
                     }}
-                    onClick={() => handleOpen(project)}
                   >
-                    View Details
-                  </Button>
-                ) : (
+                    <img
+                      src={project.image}
+                      alt={`${project.title} Logo`}
+                      style={{
+                        maxHeight: '100%',
+                        maxWidth: '100%',
+                        objectFit: 'contain',
+                      }}
+                    />
+                  </Box>
                   <Typography
-                    variant='body2'
+                    variant='h4'
+                    gutterBottom
                     sx={{
                       color:
                         theme.palette.mode === 'dark'
                           ? theme.palette.text.primary
                           : theme.palette.text.secondary,
-                      fontSize: { xs: '0.875rem', sm: '1rem' },
+                      fontWeight: theme.typography.fontWeightBold,
+                      fontSize: { xs: '1.25rem', sm: '1.5rem' },
                     }}
                   >
-                    {project.description}
+                    {project.title}
                   </Typography>
-                )}
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+                  {isMobile ? (
+                    <Button
+                      variant='contained'
+                      sx={{
+                        backgroundColor: theme.palette.threejsback.default,
+                        borderRadius: '20px',
+                        padding: '0.5rem 1.5rem',
+                        fontSize: '0.875rem',
+                        textTransform: 'none',
+                        '&:hover': {
+                          backgroundColor: theme.palette.primary.dark,
+                        },
+                      }}
+                      onClick={() => handleOpen(project)}
+                    >
+                      View Details
+                    </Button>
+                  ) : (
+                    <Typography
+                      variant='body2'
+                      sx={{
+                        color:
+                          theme.palette.mode === 'dark'
+                            ? theme.palette.text.primary
+                            : theme.palette.text.secondary,
+                        fontSize: { xs: '0.875rem', sm: '1rem' },
+                      }}
+                    >
+                      {project.description}
+                    </Typography>
+                  )}
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
-
       {openProject && (
         <Dialog
           open={!!openProject}
